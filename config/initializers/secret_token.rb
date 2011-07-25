a48267726c3624db5f8cec5a5b1826a9bac39f7f
clarencechanxy <clarencechanxy@gmail.com>
2011-07-25 12:48:33 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandwagon::Application.config.secret_token = '812b11ff260c4da2f0ba76f3ed3396340e8ada629a4118f1666dd01772c076105fa5ac0cc85a5771d822f7bddc8087de16a0a942c1a55cf2a8d16b5148b57ac7'
